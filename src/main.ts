20b77b4132f70f98584422674febe39d596e8c3c
Kurt Obando <kurtobando.developer@gmail.com>
2024-03-02 00:28:33 +0800

    setup vuefire as vue plugin

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import App from './App.vue';
 import router from './router';
 
 import { IonicVue } from '@ionic/vue';
+import { VueFire, VueFireAuth } from 'vuefire';
+import { firebaseApp } from '@/plugin/firebase/firebase';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/vue/css/core.css';
@@ -23,7 +25,13 @@ import '@ionic/vue/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
-const app = createApp(App).use(IonicVue).use(router);
+const app = createApp(App)
+    .use(IonicVue)
+    .use(VueFire, {
+        firebaseApp,
+        modules: [VueFireAuth()],
+    })
+    .use(router);
 
 router.isReady().then(() => {
     app.mount('#app');
